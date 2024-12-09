1cb80385af931912e3c2326be8ca886c84df24c0
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2024-12-09 23:39:18 +0900

    [fix] 미사용 파일 삭제 및 scale cache 동작 제거

--- deep_learning/scale.py
+++ deep_learning/scale.py
@@ -10,23 +10,20 @@ def scale_data(features, labels, character):
     return scale_features(features, character), scale_labels(labels, character)
 
 
-def scale_features(features, character):
+def scale_features(features):
     # 표준화
     sc = MaxAbsScaler()
     scaled_features = sc.fit_transform(features)
     # 결과 저장
-    np.save(f"cache/{character}_x.npy", scaled_features)
 
     return scaled_features
 
 
-def scale_labels(labels, character):
+def scale_labels(labels):
     label_copy = labels.copy()
     label_copy /= 100
     # 최대 회복, 데미지를 100으로 제한
     label_copy[label_copy < -1] = -1
     label_copy[label_copy > 1] = 1
-
-    np.save(f"cache/{character}_y.npy", label_copy)
 
     return label_copy
--- deep_learning/tune.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-import tensorflow as tf
-
-model = tf.keras.models.load_model('./model/analyze_the_spire.h5')
-x, y = np.load("./cache/scaled_x.npy"), np.load("./cache/scaled_y.npy")
-
-print(model.evaluate(x, y, verbose=2))
