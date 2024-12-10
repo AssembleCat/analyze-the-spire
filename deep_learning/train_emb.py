750d07d58061e2f7c44b11a432e13d8f829721f8
TITIBEBE <68087115+AssembleCat@users.noreply.github.com>
2024-12-10 10:31:29 +0900

    [feat] Embedding code 수정

--- deep_learning/train_emb.py
+++ deep_learning/train_emb.py
@@ -1,159 +1,224 @@
 import json
 import os
-import numpy as np
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import tensorflow as tf
-from deep_learning import preprocess, scale
-from keras import Input, Model
-from keras.src.layers import Embedding, Lambda, concatenate, Dense, Dropout
+import numpy as np
+import scale
+from keras.src.utils import pad_sequences
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MaxAbsScaler
+from keras import Input, Model
+from keras.src.layers import Embedding, Flatten, Concatenate, Dense, Dropout
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from type import sts_static
+CACHE_DIR = './cache'
 
 
-def load_data(character):
-    X = np.load(f"./cache/{character}_x.npy")
-    Y = np.load(f"./cache/{character}_y.npy")
-    return X, Y
+def load_battle_data(character):
+    BATTLE_DATA = f"../battles/clean/{character}/"
+    json_files = [file for file in os.listdir(BATTLE_DATA) if file.endswith(".json")][:45]
+    json_data = []
+
+    print(f"total file length: {len(json_files)}")
+
+    for file in json_files:
+        with open(BATTLE_DATA + file) as json_file:
+            json_data.extend(json.load(json_file))
+
+    print(f"Preprocess target battle: {len(json_data)}")
+
+    return json_data
 
 
-def avg_lambda_fun(x, mask):
-    mask_cast = tf.keras.backend.cast(mask, 'float32')
-    expanded = tf.keras.backend.expand_dims(mask_cast)
-    count = tf.keras.backend.sum(mask_cast)
-    sum = tf.keras.backend.sum(expanded * x, axis=1)
-    return sum / count
-
-
-def preprocess_data(character):
-    x, y = None, None
-    cache_data = [file for file in os.listdir("./cache") if file.endswith(".npy")]
-    if f"{character}_x.npy" in cache_data:
-        x, y = np.load(f"./cache/{character}_x.npy"), np.load(f"./cache/{character}_y.npy")
+def save_cache(character, deck_data, relic_data, enemy_data, etc_data, damage_taken, is_train=True):
+    """
+    캐시 데이터를 저장하는 함수.
+    """
+    # 캐시 파일 이름 결정
+    if is_train:
+        deck_cache = f"{character}_deck_data.npy"
+        relic_cache = f"{character}_relic_data.npy"
+        enemy_cache = f"{character}_enemy_data.npy"
+        etc_cache = f"{character}_etc_data.npy"
+        damage_cache = f"{character}_damage_cache.npy"
     else:
-        BATTLE_DATA = f"../battles/clean/{character}/"
-        json_files = [file for file in os.listdir(BATTLE_DATA) if file.endswith(".json")][:45]
-        json_data = []
+        deck_cache = f"{character}_deck_data_test.npy"
+        relic_cache = f"{character}_relic_data_test.npy"
+        enemy_cache = f"{character}_enemy_data_test.npy"
+        etc_cache = f"{character}_etc_data_test.npy"
+        damage_cache = f"{character}_damage_cache_test.npy"
 
-        print(f"total file length: {len(json_files)}")
+    cache_file_path = os.path.join(CACHE_DIR, deck_cache)
+    # 이미 캐시 파일이 존재하면 리턴
+    if os.path.exists(cache_file_path):
+        print(f"Cache already exists: {cache_file_path}")
+        return
 
-        for file in json_files:
-            with open(BATTLE_DATA + file) as json_file:
-                json_data.extend(json.load(json_file))
+    # 데이터 저장
+    np.save(os.path.join(CACHE_DIR, deck_cache), deck_data)
+    np.save(os.path.join(CACHE_DIR, relic_cache), relic_data)
+    np.save(os.path.join(CACHE_DIR, enemy_cache), enemy_data)
+    np.save(os.path.join(CACHE_DIR, etc_cache), etc_data)
+    np.save(os.path.join(CACHE_DIR, damage_cache), damage_taken)
 
-        print(f"Preprocess target battle: {len(json_data)}")
-
-        x, y = preprocess.preprocess_battle(json_data, character)
-        x, y = scale.scale_data(x, y, character)
-
-    return x, y
-
-
-def create_embedding_layers(character):
-    # 캐릭터별 카드풀
-    character_card = sts_static.get_character_cardpool(character)
-
-    # 카드 임베딩
-    card_input = Input(shape=(50,), name='cards_input')
-    card_embedding = Embedding(len(character_card) + 1, 26, mask_zero=True)(card_input)
-    card_average = Lambda(avg_lambda_fun, output_shape=(26,), mask=None)(card_embedding)
-
-    # 유물 임베딩
-    relic_input = Input(shape=(30,), name='relics_input')
-    relic_embedding = Embedding(len(sts_static.ALL_RELICS) + 1, 13, mask_zero=True)(relic_input)
-    relic_average = Lambda(avg_lambda_fun, output_shape=(13,), mask=None)(relic_embedding)
-
-    # 적 임베딩
-    enemy_input = Input(shape=(1,), name='enemy_input')
-    enemy_embedding = Embedding(len(sts_static.ALL_ENEMY), 8)(enemy_input)
-    enemy_reshape = Lambda(lambda x: tf.keras.backend.mean(x, axis=1), output_shape=(8,))(enemy_embedding)
-
-    # 숫자 및 부울 입력
-    numbers_input = Input(shape=(5,), name='num_and_bool_input')
-
-    return card_input, card_average, relic_input, relic_average, enemy_input, enemy_reshape, numbers_input
+    print(f"Cache saved to: {cache_file_path}")
 
 
-def build_model(card_average, relic_average, encounter_reshape, numbers_input):
-    merged = concatenate([card_average, relic_average, encounter_reshape, numbers_input])
-    dense_1 = Dense(500, activation='relu')(merged)
-    drop_out_1 = Dropout(.2)(dense_1)
-    dense_2 = Dense(50, activation='relu')(drop_out_1)
-    drop_out_2 = Dropout(.2)(dense_2)
-    dense_out = Dense(1)(drop_out_2)
+def load_cache(character, is_train=True):
+    """
+    캐시 파일을 불러오는 함수.
+    """
+    # 캐시 파일 경로 설정
+    if is_train:
+        deck_cache = f"{character}_deck_data.npy"
+        relic_cache = f"{character}_relic_data.npy"
+        enemy_cache = f"{character}_enemy_data.npy"
+        etc_cache = f"{character}_etc_data.npy"
+        damage_cache = f"{character}_damage_cache.npy"
+    else:
+        deck_cache = f"{character}_deck_data_test.npy"
+        relic_cache = f"{character}_relic_data_test.npy"
+        enemy_cache = f"{character}_enemy_data_test.npy"
+        etc_cache = f"{character}_etc_data_test.npy"
+        damage_cache = f"{character}_damage_cache_test.npy"
+    cache_file_path = os.path.join(CACHE_DIR, deck_cache)
 
-    return dense_out
+    # 캐시 파일이 없으면 None을 반환
+    if not os.path.exists(cache_file_path):
+        print(f"Cache not found, processing data: {cache_file_path}")
+        return None
 
+    # 캐시 파일에서 데이터 로드
+    deck_data = np.load(os.path.join(CACHE_DIR, deck_cache), allow_pickle=True)
+    relic_data = np.load(os.path.join(CACHE_DIR, relic_cache), allow_pickle=True)
+    enemy_data = np.load(os.path.join(CACHE_DIR, enemy_cache), allow_pickle=True)
+    etc_data = np.load(os.path.join(CACHE_DIR, etc_cache), allow_pickle=True)
+    damage_taken = np.load(os.path.join(CACHE_DIR, damage_cache), allow_pickle=True)
 
-def split_data(X, Y):
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, shuffle=False)
-
-    cards_col, cards_col_test = X_train[:, 0:50], X_test[:, 0:50]
-    relic_index = 50 + 30
-    relics_col, relics_col_test = X_train[:, 50:80], X_test[:, 50:80]
-    enemy_index = relic_index + 1
-    enemy_col, enemy_col_test = X_train[:, relic_index:enemy_index], X_test[:, relic_index:enemy_index]
-    num_and_bool_col, num_and_bool_col_test = X_train[:, enemy_index:], X_test[:, enemy_index:]
-
-    # 정규화
-    max_abs_scaler = MaxAbsScaler()
-    num_and_bool_col = max_abs_scaler.fit_transform(num_and_bool_col)
-
-    return cards_col, relics_col, enemy_col, num_and_bool_col, Y_train, cards_col_test, relics_col_test, enemy_col_test, num_and_bool_col_test, Y_test
-
-
-def train_model(X, Y, character):
-    # 데이터 준비
-    cards_col, relics_col, enemy_col, num_and_bool_col, Y_train, cards_col_test, relics_col_test, enemy_col_test, num_and_bool_col_test, Y_test = split_data(
-        X, Y)
-
-    # 임베딩 레이어 생성
-    card_input, card_average, relic_input, relic_average, encounter_input, encounter_reshape, numbers_input = create_embedding_layers(character)
-
-    # 모델 생성
-    dense_out = build_model(card_average, relic_average, encounter_reshape, numbers_input)
-    model = Model(inputs=[card_input, relic_input, encounter_input, numbers_input], outputs=dense_out)
-    model.summary()
-
-    # 컴파일
-    model.compile(
-        optimizer=tf.keras.optimizers.RMSprop(learning_rate=1e-4),
-        loss='mse',
-        metrics=['mae', tf.keras.metrics.R2Score()])
-
-    # 훈련
-    history = model.fit(
-        x={
-            'cards_input': cards_col,
-            'relics_input': relics_col,
-            'enemy_input': enemy_col,
-            'num_and_bool_input': num_and_bool_col
-        },
-        y=Y_train,
-        batch_size=32,
-        epochs=5,
-        validation_split=0.2
-    )
-
-    # 평가
-    test_loss, test_mae, test_r2 = model.evaluate(
-        x={
-            'cards_input': cards_col_test,
-            'relics_input': relics_col_test,
-            'enemy_input': enemy_col_test,
-            'num_and_bool_input': num_and_bool_col_test
-        },
-        y=Y_test
-    )
-    print(f"Test Loss: {test_loss}")
-    print(f"Test MAE: {test_mae}")
-    print(f"Test R² Score: {test_r2}")
-
-    return model, history
+    return [deck_data, relic_data, enemy_data, etc_data], damage_taken
 
 
-if __name__ == "__main__":
-    X, Y = preprocess_data('WATCHER')
-    trained_model, training_history = train_model(X, Y, 'WATCHER')
+def preprocess_data(data, character, card_encoder, relic_encoder, enemy_encoder, is_train=True):
+    """
+    전투 데이터를 전처리하여 캐시에 저장하고, 이미 저장된 캐시가 있으면 로드하는 함수.
+    """
+    # 캐시 로드
+    cached_data = load_cache(character, is_train)
+    character_cardpool = sts_static.get_character_cardpool(character)
+    if cached_data:
+        x, y = cached_data
+        return x, y
+
+    deck_data = []
+    relic_data = []
+    enemy_data = []
+    etc_data = []
+    damage_taken = []
+
+    for idx, battle in enumerate(data):
+        if idx % 5000 == 0:
+            print(f"{idx / len(data) * 100:.2f}% {idx+1}/{len(data)} Processing... ")
+
+        # 캐릭터 카드풀에 없는 카드는 사용하지 않도록함. -> 프리즘 조각
+        if any(card not in character_cardpool for card in battle["deck"]):
+            continue
+        # 카드 덱 라벨 인코딩
+        encoded_deck = card_encoder.transform(battle["deck"]).tolist()
+        # 유물 라벨 인코딩
+        encoded_relics = relic_encoder.transform(battle["relics"]).tolist()
+        # 적 라벨 인코딩
+        encoded_enemy = enemy_encoder.transform([battle["enemy"]])
+        # 체력 정보
+        etc_info = [battle["max_hp"], battle["entering_hp"], battle["potion_used"], battle["ascension"], battle["floor"]]
+
+        # 각 데이터에 추가
+        deck_data.append(encoded_deck)
+        relic_data.append(encoded_relics)
+        enemy_data.append(encoded_enemy)
+        etc_data.append(etc_info)
+        damage_taken.append(battle["damage_taken"])
+
+    # 패딩 처리
+    deck_data = pad_sequences(deck_data, padding='post', truncating='post', maxlen=50, dtype=np.int32)
+    relic_data = pad_sequences(relic_data, padding='post', truncating='post', maxlen=30, dtype=np.int32)
+
+    enemy_data = np.array(enemy_data, dtype=np.int32)
+    etc_data = np.array(etc_data, dtype=np.float32)
+    damage_taken = np.array(damage_taken, dtype=np.float32)
+
+    # 데미지 스케일링
+    damage_taken = scale.scale_labels(damage_taken)
+
+    # 캐시 저장
+    save_cache(character, deck_data, relic_data, enemy_data, etc_data, damage_taken, is_train)
+
+    return [deck_data, relic_data, enemy_data, etc_data], damage_taken
+
+
+character = "WATCHER"
+character_cardpool = sts_static.get_character_cardpool(character)
+card_encoder = LabelEncoder().fit(character_cardpool)
+relic_encoder = LabelEncoder().fit(sts_static.ALL_RELICS)
+enemy_encoder = LabelEncoder().fit(sts_static.ALL_ENEMY)
+
+# 데이터 로드 및 분할
+battles = load_battle_data(character)
+train_data, test_data = train_test_split(battles, test_size=0.2, random_state=42)
+
+# 전처리(인코딩)
+X_train, Y_train = preprocess_data(train_data, character, card_encoder, relic_encoder, enemy_encoder)
+X_test, Y_test = preprocess_data(test_data, character, card_encoder, relic_encoder, enemy_encoder, is_train=False)
+
+EMBEDDING_DIM = 50
+EMBEDDING_DIM_RELIC = 30
+EMBEDDING_DIM_ENEMY = 8
+
+# input shape 정의
+deck_input = Input(shape=(50,), dtype='int32', name='deck')
+relic_input = Input(shape=(30,), dtype='int32', name='relics')
+enemy_input = Input(shape=(1,), dtype='int32', name='enemy')
+etc_input = Input(shape=(5,), dtype='float32', name='etc')
+
+# 카드 임베딩
+card_embedding_layer = Embedding(input_dim=len(character_cardpool), output_dim=EMBEDDING_DIM)
+embedded_deck = card_embedding_layer(deck_input)
+embedded_deck = Flatten()(embedded_deck)
+
+# 유물 임베딩
+relic_embedding_layer = Embedding(input_dim=len(sts_static.ALL_RELICS), output_dim=EMBEDDING_DIM_RELIC)
+embedded_relics = relic_embedding_layer(relic_input)
+embedded_relics = Flatten()(embedded_relics)
+
+# 적 임베딩
+enemy_embedding_layer = Embedding(input_dim=len(sts_static.ALL_ENEMY), output_dim=EMBEDDING_DIM_ENEMY)
+embedded_enemy = enemy_embedding_layer(enemy_input)
+embedded_enemy = Flatten()(embedded_enemy)
+
+# 결합
+merged = Concatenate()([embedded_deck, embedded_relics, embedded_enemy, etc_input])
+
+dense1 = Dense(500, activation='relu')(merged)
+dropout1 = Dropout(0.2)(dense1)
+dense2 = Dense(50, activation='relu')(dropout1)
+dropout2 = Dropout(0.2)(dense2)
+output = Dense(1, activation='linear')(dropout2)
+
+# 모델 정의 및 컴파일
+model = Model(inputs=[deck_input, relic_input, enemy_input, etc_input], outputs=output)
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4), loss=tf.keras.metrics.mse, metrics=[tf.keras.metrics.R2Score()])
+
+# 모델 요약
+model.summary()
+
+# 모델 학습
+model.fit(
+    X_train,
+    Y_train,
+    epochs=5,
+    batch_size=32,
+    validation_split=0.2
+)
+
+# 테스트셋 평가
+test_loss = model.evaluate(X_test, Y_test)
+print(f"Test loss: {test_loss}")
